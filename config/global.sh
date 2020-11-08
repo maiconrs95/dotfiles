8eb0add1d569824932f2baa36fdedd36b0455a20
Maicon Silva <maicon.silva@ifood.com.br>
2020-11-08 13:33:20 -0300

    feat: initial setup

---
+++ config/global.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+source colors.sh
+
+# .bash_profile ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo "• bash_profile configuration"
+
+if [ -f "$HOME/.bash_profile" ]; then
+    msg_update ".bash_profile"
+    rm ~/.bash_profile
+else
+    msg_install ".bash_profile"
+fi
+
+sudo ln -s $DOTFILES/config/.bash_profile ~/.bash_profile
+msg_checking ".bash_profile"
+
+# .aliases :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo "• aliases configuration"
+
+if [ -f "$HOME/.aliases" ]; then
+    msg_update ".aliases"
+    rm ~/.aliases
+else
+    msg_install ".aliases"
+fi
+
+sudo ln -s $DOTFILES/config/.aliases ~/.aliases
+msg_checking ".aliases"
+
+# .gitconfig :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo "• git / github configuration"
+
+if [ -f "$HOME/.gitconfig" ]; then
+    msg_update ".gitconfig"
+    rm ~/.gitconfig
+else
+    msg_install ".gitconfig"
+fi
+
+sudo ln -s $DOTFILES/config/.gitconfig ~/.gitconfig
+msg_checking ".gitconfig"
+
+read -p "  - What is your Git user.name? " git_name
+git config --global user.name "$git_name"
+
+read -p "  - What is your Git user.email? " git_email
+git config --global user.email $git_email
+
+read -p " - What is your Git core.editor? " git_editor
+git config --global core.editor $git_editor
+
+echo ""
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source colors.sh
+
+DOTFILES="$HOME/Development/dotfiles"
+
+if [[ -d $DOTFILES ]]; then
+    print 'Checking dotfiles directory'
+else
+    print 'Cloning dotfiles'
+    git clone https://github.com/maiconrs95/dotfiles.git $DOTFILES
+fi
+
+cd $DOTFILES
+
+source config/global.sh
